7787a1770bdf395061d4309919d47848fcbe0ddd
nikiciv <nikiciv@gmail.com>
2018-04-10 14:02:29 +0000

    Added Automatic Reloading,Logging and Error Handling

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const port = process.env.PORT || 3000;
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
