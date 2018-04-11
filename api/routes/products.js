42786cf74647161d3efc847c9f65c99dbe01d9bf
nikiciv <nikiciv@gmail.com>
2018-04-11 00:12:31 +0000

    Returning responses in a prittier way.

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,10 +5,24 @@ const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('price _id name')
         .exec()
         .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        name: doc.name,
+                        price: doc.price,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + doc._id
+                        }
+                    }
+                })
+            }
+            res.status(200).json(response);
         })
         .catch(err => {
             console.log(err);
@@ -28,8 +42,16 @@ router.post('/', (req, res, next) => {
         .then(result => {
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST requests to /products',
-                createdProduct: product
+                message: 'Created product successfuly.',
+                createdProduct: {
+                    _id: result._id,
+                    name: result.name,
+                    price: result.price,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products/' + result._id
+                    }
+                }
             })
         })
         .catch(err => {
@@ -41,6 +63,7 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select('price _id name')
         .exec()
         .then(doc => {
             console.log(doc);
@@ -79,7 +102,7 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({_id: req.params.productId})
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({message: 'Product deleted.'});
         })
         .catch(err => {
             console.log(err);
